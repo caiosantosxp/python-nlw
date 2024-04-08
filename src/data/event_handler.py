583773af76296a9b230dba27d0cfb0aea444a4ca
Caio Santos <caiosantosxp@gmail.com>
2024-04-08 13:59:40 -0300

    adiconando rota de busca de usuarios

--- src/data/event_handler.py
+++ src/data/event_handler.py
@@ -17,3 +17,24 @@ class EventHandler:
          body={ "eventId": body["uuid"]},
          status_code=200
       )
+
+  def find_by_id(self, http_request: HttpRequest) -> HttpResponde:
+      event_id = http_request.param["event_id"]
+      event = self.__event_repository.get_event_by_id(event_id)
+      if not event: raise Exception("Evento nao encontrado")
+
+      event_attendees_count = self.__event_repository.count_event_attendees(event_id)
+
+      return HttpResponde(
+         body={
+            "event": {
+               "id": event.id,
+               "title": event.title,
+               "details": event.details,
+               "slug": event.slug,
+               "maximumAttendees": event.maximum_attendees,
+               "attendeesAmount": event_attendees_count["attendeesAmount"]
+            }
+         },
+         status_code=200
+      )
