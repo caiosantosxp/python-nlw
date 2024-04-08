075519c43328e3d6c281b6bb20c999a926249c83
Caio Santos <caiosantosxp@gmail.com>
2024-04-08 12:38:31 -0300

    feat: Implementing attendee repository

---
+++ src/models/repository/attendees_repository_test.py
@@ -0,0 +1,27 @@
+from src.models.settings.connection import db_connection_handler
+from src.models.repository.attendees_repository import AttendeesRepository
+import pytest
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro do Attendee em banco de dados")
+def test_insert_attendee():
+  event_id = "meu-uuid-id-teste"
+  attendees_info = {
+    "uuid": "meu_uuid_attendee",
+    "name": "attendee_name",
+    "email": "attendee@attendee.com",
+    "event_id": event_id
+  }
+  attendees_repository = AttendeesRepository()
+  response = attendees_repository.insert_attendee(attendees_info)
+  print(response)
+
+
+@pytest.mark.skip(reason="Nao necessita")
+def test_get_attendee_badge_by_id():
+  attendee_id = "meu_uuid_attendee"
+  attendees_repository = AttendeesRepository()
+  attendee = attendees_repository.get_attendee_badge_by_id(attendee_id)
+
+  print(attendee)
