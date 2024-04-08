b7b099084c6014d52d7863123ab775e634ffbe7b
Caio Santos <caiosantosxp@gmail.com>
2024-04-08 10:36:54 -0300

    feat: Implementing events_repository

---
+++ src/models/repository/events_repository_test.py
@@ -0,0 +1,26 @@
+from src.models.settings.connection import db_connection_handler
+from src.models.repository.events_repository import EventsRepository
+import pytest
+
+db_connection_handler.connect_to_db()
+
+@pytest.mark.skip(reason="Novo registro em banco de dados")
+def test_insert_event():
+  event = {
+    "uuid":"meu-uuid-id-teste",
+    "title":"meu-title-here",
+    "slug":"meu-slug-aqui",
+    "maximum_attendees":20,
+  }
+
+  events_repository = EventsRepository()
+  response = events_repository.insert_event(event)
+  print(response)
+
+
+@pytest.mark.skip(reason="Nao necessita")
+def test_get_event_by_id():
+  event_id = "meu-uuid-id-teste"
+  events_repository = EventsRepository()
+  response = events_repository.get_event_by_id(event_id)
+  print(response)
--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -2,24 +2,26 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
-  def __init__(self) -> None:
-    self.__connection_string = "{}:///{}".format(
-      "sqlite",
-      "storage.db"
-    )
-    self.__engine = None
-    self.__session = None
-
+    def __init__(self) -> None:
+        self.__connection_string = "{}:///{}".format(
+            "sqlite",
+            "storage.db"
+        )
+        self.__engine = None
+        self.session = None
+    
     def connect_to_db(self) -> None:
-      self.__engine = create_engine(self.__connection_string)
-
+        self.__engine = create_engine(self.__connection_string)
+    
     def get_engine(self):
-      return self.__engine
+        return self.__engine
     
     def __enter__(self):
-      session_maker = sessionmaker()
-      self.__session = session_maker(bind=self.__engine)
-      return self
+        session_maker = sessionmaker()
+        self.session = session_maker(bind=self.__engine)
+        return self
     
-    def __exit__(self):
-      self.__session.close()
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.session.close()
+
+db_connection_handler = DBConnectionHandler()
